5cde4007e25473b23ac09faf8d8b66a55b9d7910
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-04-16 12:01:31 +0530

    feat: make the basic dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+export default function Dashboard() {
+  return (
+    <section className="mt-20 relative flex min-h-screen items-center justify-center overflow-hidden px-4 text-center">
+      <div className="relative z-10 mx-auto max-w-5xl">
+        <h1 className="text-4xl md:text-6xl font-bold leading-tight">
+          Dashboard For the Developers
+        </h1>
+
+        <p className="mt-4 md:mt-6 text-sm md:text-xl text-foreground/60 max-w-2xl mx-auto">
+          Welcome to the central hub of the 100 ReactJS Projects platform.
+          Explore curated projects, track your progress, and discover real-world
+          applications built with React and Next.js.
+        </p>
+      </div>
+    </section>
+  );
+}
