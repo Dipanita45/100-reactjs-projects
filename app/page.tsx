3beb275fe252e472599914d8cfc6ececacb0abe5
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-04-16 21:26:13 +0530

    style: fix the style of the hero page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/hero/hero";
 
 export default function Home() {
   return (
-    <section className="max-w-6xl">
+    <section className="max-w-6xl py-25">
       <Hero />
     </section>
   );
